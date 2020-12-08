83d2733bc72efd4443f4e2a5ff54097c942c5b9e
Maximilian6547s <maximilian.s517@gmail.com>
2020-12-08 13:13:39 +0300

    Initial commit.Setup activities. Add custom font to splash screen

---
+++ app/src/main/java/com/maximcuker/projectmanagementapp/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.maximcuker.projectmanagementapp
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        val typeFace:Typeface = Typeface.createFromAsset(assets,"Cramaten.ttf")
+        tv_app_name.typeface = typeFace
+    }
+}
