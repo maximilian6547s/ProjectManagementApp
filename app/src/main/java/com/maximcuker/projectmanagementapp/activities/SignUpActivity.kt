2143dcacc48ba272d366762cbf51947aeca73eb9
Maximilian6547s <maximilian.s517@gmail.com>
2020-12-08 19:52:54 +0300

    Implement registering user via email in Firebase

--- app/src/main/java/com/maximcuker/projectmanagementapp/activities/SignUpActivity.kt
+++ app/src/main/java/com/maximcuker/projectmanagementapp/activities/SignUpActivity.kt
@@ -2,11 +2,15 @@ package com.maximcuker.projectmanagementapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.maximcuker.projectmanagementapp.R
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -14,9 +18,11 @@ class SignUpActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
-
         setupActionBar()
 
+        btn_sign_up.setOnClickListener {
+            registerUser()
+        }
     }
 
     private fun setupActionBar() {
@@ -29,4 +35,55 @@ class SignUpActivity : AppCompatActivity() {
         toolbar_sign_up_activity.setNavigationOnClickListener { onBackPressed() }
     }
 
+    private fun registerUser() {
+        val name: String = et_name.text.toString().trim { it <= ' ' }
+        val email: String = et_email.text.toString().trim { it <= ' ' }
+        val password: String = et_password.text.toString().trim { it <= ' ' }
+
+        if (validateForm(name, email, password)) {
+            //Toast.makeText(this, "We can register a new user", Toast.LENGTH_LONG).show()
+            showProgressDialog(resources.getString(R.string.please_wait))
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+                        val firebaseUser: FirebaseUser? = task.result?.user
+                        val registeredEmail = firebaseUser?.email
+                        Toast.makeText(
+                            this,
+                            "$name,you have successfully registered the email address $registeredEmail",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        FirebaseAuth.getInstance().signOut()
+                        finish()
+                    } else {
+                        Toast.makeText(
+                            this,
+                            task.exception?.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+
+    private fun validateForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please enter a name")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please enter a email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please enter a password")
+                false
+            }
+            else -> {
+                true
+            }
+        }
+    }
 }
