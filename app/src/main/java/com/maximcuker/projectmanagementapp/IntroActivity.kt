dbbe9c92c7e207dbfbd60fbfdaa31e83113ace22
Maximilian6547s <maximilian.s517@gmail.com>
2020-12-08 16:44:37 +0300

    Setup sign in and sign up activities. Need to rework in one SignInSignUp activity

--- app/src/main/java/com/maximcuker/projectmanagementapp/IntroActivity.kt
+++ app/src/main/java/com/maximcuker/projectmanagementapp/IntroActivity.kt
@@ -1,8 +1,10 @@
 package com.maximcuker.projectmanagementapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +12,13 @@ class IntroActivity : AppCompatActivity() {
         setContentView(R.layout.activity_intro)
 
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        btn_sign_up_intro.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+
+        btn_sign_in_intro.setOnClickListener {
+            startActivity(Intent(this,SignInActivity::class.java))
+        }
 
     }
 }
