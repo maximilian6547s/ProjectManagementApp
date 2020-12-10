716b2c768988179ae8fe6cca0122c16f1e447954
Maximilian6547s <maximilian.s517@gmail.com>
2020-12-10 12:32:48 +0300

    Prepare profile activity activity_my_profile.xml

---
+++ app/src/main/java/com/maximcuker/projectmanagementapp/activities/MyProfileActivity.kt
@@ -0,0 +1,29 @@
+package com.maximcuker.projectmanagementapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.maximcuker.projectmanagementapp.R
+import kotlinx.android.synthetic.main.activity_my_profile.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.app_bar_main.toolbar_main_activity
+
+class MyProfileActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_profile)
+        setupActionBar()
+    }
+
+    private fun setupActionBar() {
+        setSupportActionBar(toolbar_my_profile_activity)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title = resources.getString(R.string.my_profile_title)
+        }
+        toolbar_my_profile_activity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+    }
+}
