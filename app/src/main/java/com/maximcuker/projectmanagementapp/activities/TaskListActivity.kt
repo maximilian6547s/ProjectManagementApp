6a7ecd86d7c77280f3b398754d001cb562d10fbd
Maximilian6547s <maximilian.s517@gmail.com>
2020-12-15 17:42:39 +0300

    Prepare activity_members.xml

--- app/src/main/java/com/maximcuker/projectmanagementapp/activities/TaskListActivity.kt
+++ app/src/main/java/com/maximcuker/projectmanagementapp/activities/TaskListActivity.kt
@@ -1,6 +1,9 @@
 package com.maximcuker.projectmanagementapp.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.maximcuker.projectmanagementapp.R
 import com.maximcuker.projectmanagementapp.adapters.TaskListItemsAdapter
@@ -26,6 +29,22 @@ class TaskListActivity : BaseActivity() {
 
         showProgressDialog(resources.getString(R.string.please_wait))
         FirestoreClass().getBoardDetails(this, boardDocumentId)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_members, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_members -> {
+                val intent = Intent(this, MembersActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
     private fun setupActionBar() {
