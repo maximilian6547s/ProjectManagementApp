595c3acbb81721603cc7b8e3fbc3a65e0ac225eb
Maximilian6547s <maximilian.s517@gmail.com>
2020-12-15 16:39:45 +0300

    Add card to task in firestore

--- app/src/main/java/com/maximcuker/projectmanagementapp/activities/TaskListActivity.kt
+++ app/src/main/java/com/maximcuker/projectmanagementapp/activities/TaskListActivity.kt
@@ -6,6 +6,7 @@ import com.maximcuker.projectmanagementapp.R
 import com.maximcuker.projectmanagementapp.adapters.TaskListItemsAdapter
 import com.maximcuker.projectmanagementapp.firebase.FirestoreClass
 import com.maximcuker.projectmanagementapp.models.Board
+import com.maximcuker.projectmanagementapp.models.Card
 import com.maximcuker.projectmanagementapp.models.Task
 import com.maximcuker.projectmanagementapp.utils.Constants
 import kotlinx.android.synthetic.main.activity_task_list.*
@@ -65,7 +66,7 @@ class TaskListActivity : BaseActivity() {
     }
 
     fun createTaskList(taskListName: String) {
-        val task = Task(taskListName, FirestoreClass().getCurrentUserId()!!)
+        val task = Task(taskListName, FirestoreClass().getCurrentUserId().toString())
         mBoardDetails.taskList.add(0, task)
         removeLastElementOFTaskList()
 
@@ -91,7 +92,26 @@ class TaskListActivity : BaseActivity() {
         FirestoreClass().addUpdateTaskList(this,mBoardDetails)
     }
 
-    fun removeLastElementOFTaskList() {
+    fun addCardToTaskList(position: Int, cardName:String) {
+        removeLastElementOFTaskList()
+        val cardAssignedUsersList: ArrayList<String> = ArrayList()
+        val currentUserID = FirestoreClass().getCurrentUserId().toString()
+        val currentTask = mBoardDetails.taskList[position]
+        cardAssignedUsersList.add(currentUserID)
+        val card = Card(cardName,currentUserID,cardAssignedUsersList)
+
+        val cardsList = currentTask.cards
+        cardsList.add(card)
+
+        val task = Task(currentTask.title, currentTask.createdBy, cardsList)
+
+        mBoardDetails.taskList[position] = task
+        showProgressDialog(resources.getString(R.string.please_wait))
+        FirestoreClass().addUpdateTaskList(this,mBoardDetails)
+
+    }
+
+    private fun removeLastElementOFTaskList() {
         mBoardDetails.taskList.removeAt(mBoardDetails.taskList.size-1)
     }
 
