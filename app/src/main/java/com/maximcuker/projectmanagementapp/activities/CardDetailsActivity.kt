6bb346d5acc3ffdccf38294ec38d4c636c472d91
Maximilian6547s <maximilian.s517@gmail.com>
2020-12-17 13:19:24 +0300

    select label color and update card in firestore

--- app/src/main/java/com/maximcuker/projectmanagementapp/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/maximcuker/projectmanagementapp/activities/CardDetailsActivity.kt
@@ -2,12 +2,14 @@ package com.maximcuker.projectmanagementapp.activities
 
 import android.app.Activity
 import android.app.AlertDialog
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import com.maximcuker.projectmanagementapp.R
+import com.maximcuker.projectmanagementapp.dialogs.LabelColorListDialog
 import com.maximcuker.projectmanagementapp.firebase.FirestoreClass
 import com.maximcuker.projectmanagementapp.models.Board
 import com.maximcuker.projectmanagementapp.models.Card
@@ -18,6 +20,7 @@ import kotlinx.android.synthetic.main.activity_members.*
 
 class CardDetailsActivity : BaseActivity() {
 
+    private var mSelectedColor: String = ""
     private lateinit var mBoardDetails: Board
     private var mTaskListPosition = -1
     private var mCardPosition = -1
@@ -38,6 +41,15 @@ class CardDetailsActivity : BaseActivity() {
                 Toast.makeText(this, "Please enter a name of the card", Toast.LENGTH_LONG).show()
             }
         }
+
+        tv_select_label_color.setOnClickListener {
+            labelColorsListDialog()
+        }
+    }
+
+    private fun setColor() {
+        tv_select_label_color.text = ""
+        tv_select_label_color.setBackgroundColor(Color.parseColor(mSelectedColor))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -93,7 +105,8 @@ class CardDetailsActivity : BaseActivity() {
         val card = Card(
             et_name_card_details.text.toString(),
             mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].createdBy,
-            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo
+            mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition].assignedTo,
+            mSelectedColor
         )
         mBoardDetails.taskList[mTaskListPosition].cards[mCardPosition] = card
         showProgressDialog(resources.getString(R.string.please_wait))
@@ -134,6 +147,18 @@ class CardDetailsActivity : BaseActivity() {
         val alertDialog: AlertDialog = builder.create()
         alertDialog.setCancelable(false)
         alertDialog.show()
+    }
+
+    private fun labelColorsListDialog() {
+        val colorList: ArrayList<String> = Constants.colorsList()
+
+        val listDialog = object: LabelColorListDialog(this, colorList, resources.getString(R.string.str_select_label_color)){
+            override fun onItemSelected(color: String) {
+                mSelectedColor = color
+                setColor()
+            }
+        }
+        listDialog.show()
     }
 
 
--- app/src/main/java/com/maximcuker/projectmanagementapp/utils/Constants.kt
+++ app/src/main/java/com/maximcuker/projectmanagementapp/utils/Constants.kt
@@ -26,6 +26,18 @@ object Constants {
     const val TASK_LIST_ITEM_POSITION = "task_list_item_position"
     const val CARD_LIST_ITEM_POSITION = "card_list_item_position"
 
+    //TODO refactor
+    fun colorsList():ArrayList<String> {
+        val colorsList = ArrayList<String>()
+        colorsList.add("#43C86F")
+        colorsList.add("#0C90F1")
+        colorsList.add("#F72400")
+        colorsList.add("#7A8089")
+        colorsList.add("#D57C1D")
+        colorsList.add("#770000")
+        colorsList.add("#0022F8")
+        return colorsList
+    }
 
     fun showImageChooser(activity: Activity) {
         val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
